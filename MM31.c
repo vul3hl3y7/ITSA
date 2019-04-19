6ba2b5bcaf3be8000c659e87a1aad63ea133e0c9
Lasto <ejo3t965ek@gmail.com>
2019-04-20 01:41:45 +0800

    Create MM31.c

---
+++ MM31.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n,i,j,a;
+	int b = 1;
+	scanf("%d", &n);
+
+	for(i = 1;i <= n; i++)
+	{
+		scanf("%d",&a);
+		for(j = 1;j <= a;j++)
+		{
+			b = j * b;
+		}
+		printf("%d\n",b);
+		b = 1;
+	}
+	return 0;
+}
