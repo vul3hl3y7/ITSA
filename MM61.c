09c2d9d8d58bb801c695e440708d0d50ceb93110
Lasto <ejo3t965ek@gmail.com>
2019-04-20 01:42:17 +0800

    Create MM61.c

---
+++ MM61.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int i,x,y,z,a,b,c;
+	while(scanf("%d %d %d",&x,&y,&z) != EOF)
+	{
+		if(x < 0 || y < 0 || z < 0)
+		{
+			printf("No solution\n");
+			continue;
+		}
+		c = x - z;	//c = crab
+		i = y - 8 * c;	//2a+4b = i
+		b = (i - 2 * z) / 2;	//a = chicken
+		a = x - b - c;	//b = rabbit
+		if(b % 2 != 0 || a < 0 || b < 0 || c < 0)
+		{
+				printf("No solution\n");
+				continue;
+		}
+		printf("%d %d %d\n",a,b,c);
+	}
+
+	return 0;
+}
