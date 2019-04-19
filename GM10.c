d33d60eb54884a97ec58a505b026620b329b1daf
Lasto <ejo3t965ek@gmail.com>
2019-04-20 01:41:36 +0800

    Create GM10.c

---
+++ GM10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+		int n,i,X,Y,x,y;
+		scanf("%d", &n);
+		for(i = 0;i < n;i++)
+		{
+				scanf("%d %d %d %d",&X,&Y,&x,&y);
+				if(X > 0 && Y > 0 && x > 0 && y < 0)
+						printf("X\n");
+				else if(X > 0 && Y > 0 && x < 0 && y > 0)
+						printf("Y\n");
+				else if(X > 0 && Y > 0 && x < 0 && y < 0)
+						printf("Both\n");
+				else if(X < 0 && Y > 0 && x > 0 && y < 0)
+						printf("Both\n");
+				else if(X > 0 && Y < 0 && x < 0 && y < 0)
+						printf("Y\n");
+				else if(X < 0 && Y > 0 && x < 0 && y < 0)
+						printf("X\n");
+				else if(X < 0 && Y > 0 && x > 0 && y > 0)
+						printf("Y\n");
+				else if(X > 0 && Y < 0 && x > 0 && y > 0)
+						printf("X\n");
+				else if(X > 0 && Y < 0 && x < 0 && y > 0)
+						printf("Both\n");
+				else if(X < 0 && Y < 0 && x > 0 && y < 0)
+						printf("Y\n");
+				else if(X < 0 && Y < 0 && x > 0 && y > 0)
+						printf("Both\n");
+				else if(X < 0 && Y < 0 && x < 0 && y > 0)
+						printf("X\n");
+				else
+						printf("None\n");
+		}
+		return 0;
+}
